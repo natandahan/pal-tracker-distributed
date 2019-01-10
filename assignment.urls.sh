1bc4eb494d713d94a726c6aefbf8ea18b5f2fd06
ntnd13 <ntnd13@gmail.com>
2019-01-10 13:57:31 -0700

    edit/manifest registration server endpoint

--- assignment.urls.sh
+++ assignment.urls.sh
@@ -2,6 +2,13 @@ REGISTRATION_APP_URL=registration-pal-alanatan.apps.evans.pal.pivotal.io
 BACKLOG_APP_URL=backlog-pal-alanatan.apps.evans.pal.pivotal.io
 ALLOCATIONS_APP_URL=allocations-pal-alanatan.apps.evans.pal.pivotal.io
 TIMESHEETS_APP_URL=timesheets-pal-alanatan.apps.evans.pal.pivotal.io
+
+echo "curl -i -XPOST -H"Content-Type: application/json" https://${REGISTRATION_APP_URL}:8083/registration -d'{"name": "Pete"}'"
+echo "curl -i https://${REGISTRATION_APP_URL}:8083/users/${USER_ID}"
+
+
+
+
 cd ~/workspace/assignment-submission
 echo "./gradlew cloudNativeDeveloperDistributedSystemDeployment \
     -PregistrationServerUrl=https://${REGISTRATION_APP_URL} \
